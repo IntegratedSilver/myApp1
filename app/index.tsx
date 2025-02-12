b74169244ff69c085808f3623ddd8f698d6abf91
Alejandro De La Cruz-Rubio <a.d.l.c.rubio@gmail.com>
2025-02-11 20:11:57 -0800

    p

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,37 @@
-import { Text, View, StyleSheet, } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function Index() {
-
-  const handlePress = () => console.log("pressed from a function")
+  const handlePress = () => console.log("pressed from a function");
 
   return (
-    <View
-      style={styles.container}
-    >
-      <Text numberOfLines={1} onPress={handlePress}>Edit app/index.tsx to edit this screen. Lets add some more text here</Text>
+    <View style={[styles.container]}>
+      <Button
+        color={"orange"}
+        title="Click me!"
+        onPress={() => Alert.prompt("My Title", "My Message", text => console.log(text))}
+      />
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'}/>
     </View>
   );
 }
 
+const backgroundStyle = {BackgroundColor: 'orange'}
+
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    
-    backgroundColor: "cornflowerblue"
-  }
-})
+    padding: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  },
+});
